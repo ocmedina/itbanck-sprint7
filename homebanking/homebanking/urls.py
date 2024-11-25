edd837f3f1c187dec91d5eeeeba4abc7bd69789f
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-11-25 20:49:13 -0300

    test

--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('login/', include('login.urls'))
 ]
